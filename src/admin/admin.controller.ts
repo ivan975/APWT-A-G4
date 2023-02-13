048603e1c4f8ae22e4a1fa30f871f7ebfbe2084c
username-unavailable-is-not-available <yeasinelahi04@gamil.com>
2023-02-13 23:26:46 +0600

    -

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+
+    @Get()
+    SendMsg(): string {
+        return 'Welcome ADMIN!'
+    }
+
+    @Get('/users')
+    DemoMsg(): string {
+        return 'USERS'
+    }
+    
+    @Get('/users/employee')
+    dm1(): string {
+        return 'List of Employees'
+    }
+
+    @Get('/users/publisher')
+    dm2(): string {
+        return 'List of Publishers'
+    }
+
+    @Get('/users/user')
+    dm3(): string {
+        return 'List of Active Users'
+    }
+}
